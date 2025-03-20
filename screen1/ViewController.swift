01475bda8de54c516d59e6891a36380cabbf4fa0
PetrushenkoDmitry <petrushenkodmitrii@yandex.ru>
2025-03-20 23:41:22 +0300

    Initial Commit

---
+++ screen1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  screen1
+//
+//  Created by Дмитрий Петрушенко on 20/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
